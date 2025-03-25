15a8d2f02f5351fb88affc25d91eab9c1ccc684b
InobatMi <inobatmirahmedova@gmail.com>
2025-03-25 23:38:44 +0500

    feat: 19 hw

--- lesson-16/sqlite_practice.py
+++ lesson-16/sqlite_practice.py
@@ -7,7 +7,6 @@ cursor = conn.cursor()
 crt_qr = 'create table employee (id int, name varchar(30), age int)'
 
 insert_qr = '''insert into employee values (1, 'Alex', 26)'''
-
 cursor.execute(insert_qr)
 
 conn.commit()
